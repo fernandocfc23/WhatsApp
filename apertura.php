5af1e887869eaf44660a1d73f8d1f5998167d8de
fg <fernandocfc22@outlook.com>
2018-03-23 14:22:01 -0600

    CONVERSACION INDIVIDUAL
    Cambia según la conversación

--- apertura.php
+++ apertura.php
@@ -1,11 +1,44 @@
 <?php 
+	$receptor = $_POST['receptor'];
 	$fp = fopen("mensajes.txt", "r");
 	$i=0;
 	while(!feof($fp)) {
-		$linea[$i] = fgets($fp);
-		echo $linea[$i];
-		$i++;
+		$porciones = explode(",", fgets($fp));
+		$receptorG = explode(":",$porciones[0]);
+		$mensaje = explode(":", $porciones[1]);
+		if($receptor=="fg")
+		{
+			echo '<div class="row message-body">
+            <div class="col-sm-12 message-main-receiver">
+              <div class="receiver">
+                <div class="message-text">';
+				echo $mensaje[1];
+                echo '</div>
+                <span class="message-time pull-right">
+                  18:18
+                </span>
+              </div>
+            </div>
+          </div>';
+		}
+		if($receptor==$receptorG[1])
+		{
+	        echo '<div class="row message-body"> 
+					<div class="col-sm-12 message-main-sender">
+					<div class="sender">
+					<div class="message-text">';
+					echo $mensaje[1];
+					echo '</div>
+					<span class="message-time pull-right">
+					18:19
+					</span>
+					</div>
+					</div>
+					</div>';
+			echo '<br/>';
+			$i++;
+		}
 	}
 	fclose($fp);
-	json_encode($linea);
+
 ?>
--- js/controlador.js
+++ js/controlador.js
@@ -33,11 +33,17 @@ function activar(nombreContenedor){
 	var nomDiv = ultAct.concat(ultAct2);
 	$(ultAct).css("background-color", "#1b8df1");
 	$(nomDiv).css("color","white");
+	var r = comprobarReceptor(ultAct);
+	$("#nomContacto").text(r);
+	var parametros= {
+		"receptor": r
+	};
 	$.ajax({
+			data:  parametros, 
 	        url:   'apertura.php', 
 	        type:  'post', 
         	success : function (data) {
-        		console.dir(data);
+        		$("#mensajes0").append(data);
         	}
     });
 }
@@ -46,6 +52,7 @@ function desactivar(res){
 	var nomDiv2 = res.concat(ultAct2);
 	$(res).css("background-color", "white");
 	$(nomDiv2).css("color", "black");
+	$("#mensajes0").empty();
 }
 
 function comprobarReceptor(ultAct)
